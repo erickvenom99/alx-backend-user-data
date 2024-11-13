f82f7e6047962ed665ebb33b9d5d364de6bba14d
ericvenom99 <prevenakhtar@mail.com>
2024-11-13 14:08:08 +0100

    update python index file

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', methods=['GET'])
+@app_views.route('/api/v1/unauthorized', methods=['GET'])
 def unauthorized_handler():
     """
       unauthorized route
@@ -34,7 +34,7 @@ def unauthorized_handler():
     abort(401)
 
 
-@app_views.route('/forbidden', methods=['GET'])
+@app_views.route('/api/v1/forbidden', methods=['GET'])
 def forbidden_error():
     """
     Raises a 403 Forbidden error
