c3a74510c50af1ff3c871828d3fa1a86c745860c
ericvenom99 <prevenakhtar@mail.com>
2024-11-16 05:02:07 +0100

    modified basic_auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@ MODULE MANAGES  BASIC AUTHENTICATION
 """
 from api.v1.auth.auth import Auth
 from typing import TypeVar
+from models.user import User
+import base64
 
 
 class BasicAuth(Auth):
@@ -11,4 +13,20 @@ class BasicAuth(Auth):
     Basic_Auth implements Api authentication
 
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+           Retrieve the Base64 part of the Authorization
+              header
+           Args:
+               authorization_header: the header string
+            Returns:
+                the value after Basic(after the space) or None
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
