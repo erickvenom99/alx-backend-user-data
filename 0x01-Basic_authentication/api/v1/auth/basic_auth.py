e4821e25313c2d8b9ea8576aacabfcd44f040a55
ericvenom99 <prevenakhtar@mail.com>
2024-11-16 06:10:46 +0100

    update basic_auth with a new method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 MODULE MANAGES  BASIC AUTHENTICATION
-"""
+0;10;1c"""
 from api.v1.auth.auth import Auth
 from typing import TypeVar
 from models.user import User
@@ -30,3 +30,24 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+           Retrieve the Base64 part of the Authorization
+              header
+           Args:
+               authorization_header: the header string
+            Returns:
+                the value after Basic(after the space) or None
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_string = base64.b64decode(
+                base64_authorization_header).decode('utf-8')
+            return decoded_string
+        except Exception as e:
+            return None
