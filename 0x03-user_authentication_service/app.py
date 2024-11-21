c781fc0ee1d20c126d35a385f6fe4535bf8cebba
ericvenom99 <prevenakhtar@mail.com>
2024-11-21 18:27:33 +0100

    correct the login in app.py for reset_password method

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -98,19 +98,15 @@ def update_password():
     """
     Updates a user's password using a reset token.
     """
-    email = request.form.get('email')
-    reset_token = request.form.get('reset_token')
-    new_password = request.form.get('new_password')
-
-    if not email or not reset_token or not new_password:
-        abort(403)
-
     try:
+        email = request.form.get('email')
+        reset_token = request.form.get('reset_token')
+        new_password = request.form.get('new_password')
+
         AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
     except ValueError:
         abort(403)
-
-    return jsonify({"email": email, "message": "Password updated"}), 200
 
 
 if __name__ == "__main__":
