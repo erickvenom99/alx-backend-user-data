f96fde7f8fed324474b6bc03e10f8d5c31f51d91
ericvenom99 <prevenakhtar@mail.com>
2024-11-21 14:35:05 +0100

    corrected code

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -76,3 +76,24 @@ class Auth:
             return False
         except (NoResultFound, InvalidRequestError):
             return False
+
+    def create_session(self, email: str) -> str:
+        """
+        Create a session for a user.
+
+        Args:
+            email (str): User email.
+
+        Returns:
+            str: The session ID.
+
+        Raises:
+            ValueError: If the user is not found.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            raise ValueError("User not found")
