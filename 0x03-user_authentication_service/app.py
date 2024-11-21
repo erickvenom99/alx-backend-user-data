f6507c7ae734b375614f45dbea993d50d4443f6e
ericvenom99 <prevenakhtar@mail.com>
2024-11-21 16:31:10 +0100

    updated method to auth.py and app.py files

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -78,5 +78,20 @@ def profile():
         abort(403)
 
 
+@app.route('/reset_passwords', methods=['POST'], strict_slashes=False)
+def get_reset_password_token():  # Corrected function name
+    """
+    Generates a reset password token for the given email.
+    """
+    email = request.form.get('email')
+    if not email:
+        abort(403)
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -142,8 +142,8 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
-            raise ValueError("User not found")
-
-        reset_token = self._generate_uuid()
+            raise ValueError
+        reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
+
