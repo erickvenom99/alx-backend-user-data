946ebebfb1e2ecc173a9c25f4868630cecd121b7
ericvenom99 <prevenakhtar@mail.com>
2024-11-21 15:36:46 +0100

    added destroy_session method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -115,3 +115,13 @@ class Auth:
             return user
         except (NoResultFound, InvalidRequestError):
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """
+        method remove sesssions
+        Args:
+            user_id (int): user ID.
+        Returns:
+            None
+        """
+        return self._db.update_user(user_id, session_id=None)
