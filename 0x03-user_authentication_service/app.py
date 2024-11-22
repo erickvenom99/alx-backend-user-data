f0f753f15389b5ca9aef52ab579011c2621d27ca
ericvenom99 <prevenakhtar@mail.com>
2024-11-22 05:11:54 +0100

    update the update_password method

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -93,7 +93,7 @@ def get_reset_password_token():
         abort(403)
 
 
-@app.route('/reset_passwords', methods=['PUT'], strict_slashes=False)
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
 def update_password() -> str:
     """
     Updates a user's password using a reset token.
